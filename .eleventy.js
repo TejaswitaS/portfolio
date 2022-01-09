d24628befd9f53b2da030450eaf1d9ed30571f37
Tejaswita Singh <tejaswitasingh93@gmail.com>
2022-01-09 17:42:00 +0530

    Added file

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,15 @@ module.exports = function (eleventyConfig) {
     // Copy the `img` and `css` folders to the output
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css");
+
+    return {
+		pathPrefix: "/", // useful for GitHub pages
+		dir: {
+			input: "./",
+			output: "dist",
+			includes: "src/includes",
+			layouts: "src/layouts",
+			data: "src/data"
+		}
+	};
 }
